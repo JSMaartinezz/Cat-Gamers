f2d05f63fe052c73d7cd20535d2178398f33da81
Javier <javier.saurin.martinez@gmail.com>
2026-09-03 18:32:40 +0200

    feat: seccion tienda añadida

---
+++ Cat Gamers/js/tienda.js
@@ -0,0 +1,11 @@
+// Variables globales //
+let mainContainer = document.querySelector(".main-container");
+let tarjetaProducto = document.querySelector(".tarjeta-producto");
+let imagenProducto = document.querySelector(".imagen-producto");
+let tituloProducto = document.querySelector(".titulo-producto");
+let listaDetallesProducto = document.querySelector(".lista-detalles-producto");
+let descripcionProducto = document.querySelector(".descripcion-producto");
+let categoriaProducto = document.querySelector(".categoria-producto");
+let precioProducto = document.querySelector(".precio-producto");
+let botonTarjeta = document.querySelector(".añadir-carrito");
+
