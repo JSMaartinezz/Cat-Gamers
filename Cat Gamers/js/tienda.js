dd471076a3e80bfd8913f0b2cdb3c3d05d982b1c
Javier <javier.saurin.martinez@gmail.com>
2026-09-03 19:41:09 +0200

    style: seccion tienda hecha

--- Cat Gamers/js/tienda.js
+++ Cat Gamers/js/tienda.js
@@ -1,5 +1,6 @@
 // Variables globales //
 let mainContainer = document.querySelector(".main-container");
+let contenedorMain = document.getElementById("contenedor-main");
 let tarjetaProducto = document.querySelector(".tarjeta-producto");
 let imagenProducto = document.querySelector(".imagen-producto");
 let tituloProducto = document.querySelector(".titulo-producto");
@@ -9,3 +10,24 @@ let categoriaProducto = document.querySelector(".categoria-producto");
 let precioProducto = document.querySelector(".precio-producto");
 let botonTarjeta = document.querySelector(".añadir-carrito");
 
+fetch(
+    'data/productos.json')
+.then(respuesta => respuesta.json())
+.then(productos => {
+    productos.forEach(producto => {
+    const tarjetaHTML = `
+    <div class="tarjeta-producto">
+      <img class="imagen-producto" src="${producto.imagen}" alt="${producto.nombre}">
+      <h2 class="titulo-producto">${producto.nombre}</h2>
+      <ul class="lista-detalles-producto">
+        <li class="descripcion-producto">${producto.descripcion}</li>
+        <li class="categoria-producto">${producto.categoria}</li>
+        <li class="precio-producto">${producto.precio}€</li>
+      </ul>
+      <button class="añadir-carrito">Añadir al carrito</button>
+    </div>
+    `;
+    contenedorMain.innerHTML += tarjetaHTML;
+    });
+})
+.catch(error => console.error("Error al cargar:", error));
